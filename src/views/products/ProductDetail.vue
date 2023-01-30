2f62525a88bb546a7608ee64453a07a0e42c6c13
Helen <helengreent23@gmail.com>
2023-01-31 01:34:21 +0200

    AV-rewrite productDetail-DRY

--- src/views/products/ProductDetail.vue
+++ src/views/products/ProductDetail.vue
@@ -1,154 +1,66 @@
-he<template>
+<template>
   <section class="container">
-    <template v-if="editMode">
-      <div
-        v-loading="pending"
-        class="product"
-      >
-        <div class="w-full flex justify-center">
-          <img :src="product?.image_url" :alt="product?.title" class="max-h-[600px]">
+    <div
+      v-loading="pending"
+      class="product"
+    >
+      <div class="w-full flex justify-center">
+        <img :src="product?.image_url" :alt="product?.title" class="max-h-[600px]">
+      </div>
+      <div class="sm:px-6 lg:px-14 lg:pt-[28px] px-10">
+        <div v-if="editMode" class="mb-4">
+          <img src="@/assets/icons/pencil.svg" class="w-[15px] mr-3 mb-2" alt="edit">
+          <input
+            v-model="payload.title"
+            type="text"
+            class="md:text-6 font-clash text-4xl leading-[44px] text-violet-color"
+          >
         </div>
+        <h2 v-else class="md:text-6 font-clash text-4xl leading-[44px] mb-4 text-violet-color">
+          {{ product?.title }}
+          <span>
+            <img
+              v-if="user?.user_role === 'admin'"
+              src="@/assets/icons/pencil.svg"
+              class="w-[20px] ml-3 pb-2 cursor-pointer"
+              alt="edit"
+              @click="editMode = true"
+            >
+            <img
+              v-if="user?.user_role === 'admin'"
+              src="@/assets/icons/plus.svg"
+              class="w-[30px] ml-3 pb-2 cursor-pointer"
+              alt="add-product"
+              @click="createNewProduct"
+            >
+          </span>
+        </h2>
 
-        <div class="sm:px-6 lg:px-14 lg:pt-[28px] px-10">
-          <div class="mb-4">
-            <img src="@/assets/icons/pencil.svg" class="w-[15px] mr-3 mb-2" alt="edit">
+        <div v-if="editMode" class="flex justify-start items-center mb-7 md:text-[20px] text-2xl ">
+          <img src="@/assets/icons/pencil.svg" class="w-[15px] mr-3" alt="edit">
+          <span class="pr-1">£</span>
+          <input
+            v-model="payload.price"
+            type="text"
+            class="text-[#12131A]"
+          >
+        </div>
+        <span v-else class="md:text-[20px] block text-2xl mb-7 text-[#12131A]">£{{ product?.price }}</span>
+
+        <div class="text-grey-violet space-y-3">
+          <span class="font-clash text-violet-color mb-4 leading-5">Description</span>
+          <div v-if="editMode" class="flex items-center">
+            <img src="@/assets/icons/pencil.svg" class=" w-[15px] mr-3 mb-5" alt="edit">
             <input
-              v-model="payload.title"
-              type="text"
-              class="md:text-6 font-clash text-4xl leading-[44px] text-violet-color"
-            >
-          </div>
-          <div class="flex justify-start items-center mb-7 md:text-[20px] text-2xl ">
-            <img src="@/assets/icons/pencil.svg" class="w-[15px] mr-3" alt="edit">
-            <span class="pr-1">£</span>
-            <input
-              v-model="payload.price"
-              type="text"
-              class="text-[#12131A]"
-            >
-          </div>
-          <div class="text-grey-violet space-y-3">
-            <span class="font-clash text-violet-color mb-4 leading-5">Description :</span>
-            <div class="flex items-center">
-              <img src="@/assets/icons/pencil.svg" class=" w-[15px] mr-3 mb-5" alt="edit">
-              <input
-                v-model="payload.description"
-                type="textarea"
-                class="w-full min-h-[50px] p-2 block mb-[20px] leading-[22px]"
-                placeholder="A timeless design, with premium materials features as one of our most popular and
+              v-model="payload.description"
+              type="textarea"
+              class="w-full min-h-[50px] p-2 block mb-[20px] leading-[22px]"
+              placeholder="A timeless design, with premium materials features as one of our most popular and
                 iconic pieces. The dandy chair is perfect for any stylish living space with beech
                 legs and lambskin leather upholstery."
-              >
-            </div>
-          </div>
-          <div class="mt-12 mb-4">
-            <span class="font-clash text-violet-color mb-4 leading-5">Dimensions :</span>
-            <div class="pt-6 flex justify-between max-w-[241px] text-violet-color">
-              <div class="space-y-3">
-                <span class="font-clash text-sm text-grey-violet">Width :</span>
-                <div class="flex">
-                  <img src="@/assets/icons/pencil.svg" class=" w-[10px] mr-1" alt="edit">
-                  <input
-                    v-model="payload.width"
-                    type="text"
-                  >
-                </div>
-              </div>
-              <div class="space-y-3">
-                <span class="font-clash text-sm text-grey-violet">Height :</span>
-                <div class="flex">
-                  <img src="@/assets/icons/pencil.svg" class=" w-[10px] mr-1" alt="edit">
-                  <input
-                    v-model="payload.height"
-                    type="text"
-                  >
-                </div>
-              </div>
-              <div class="space-y-3">
-                <span class="font-clash text-sm text-grey-violet">Depth :</span>
-                <div class="flex">
-                  <img src="@/assets/icons/pencil.svg" class=" w-[10px] mr-1" alt="edit">
-                  <input
-                    v-model="payload.depth"
-                    type="text"
-                  >
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="sm:flex-col lg:mb-4 flex justify-between mt-[27px] space-y-3.5">
-            <div class="sm:flex-col flex justify-left space-y-3.5 font-clash text-violet-color">
-              <span class="sm:mt-0 w-[130px] pt-[13px] pr-[22px] mt-[13px] leading-5">Quantity:</span>
-              <div class="sm:w-full w-[122px] h-[46px] flex justify-center items-center">
-                <img src="@/assets/icons/pencil.svg" class=" w-[10px] mr-1" alt="edit">
-                <input
-                  v-model="payload.qty"
-                  type="text"
-                >
-              </div>
-            </div>
-            <el-button
-              :type="$elComponentType.primary"
-              class="sm:w-full w-[143px] h-[56px] font-satoshi font-normal text-base"
-              @click="basketStore.addProducts(product as IBasketProduct, quantity)"
             >
-              Add to cart
-            </el-button>
           </div>
-        </div>
-      </div>
-
-      <div class="flex justify-center">
-        <img
-          v-if="user?.user_role === 'admin'"
-          src="@/assets/icons/cancel.svg"
-          alt="cancel"
-          class="cursor-pointer"
-          @click="cancel"
-        >
-        <img
-          v-if="user?.user_role === 'admin'"
-          src="@/assets/icons/checkmark_green.svg"
-          alt="update"
-          class="cursor-pointer"
-          @click="handleUpdate(product?.id as string)"
-        >
-      </div>
-    </template>
-
-    <!-- without edit mode -->
-    <template v-else>
-      <div
-        v-loading="pending"
-        class="product"
-      >
-        <div class="w-full flex justify-center">
-          <img :src="product?.image_url" :alt="product?.title" class="max-h-[600px]">
-        </div>
-        <div class="sm:px-6 lg:px-14 lg:pt-[28px] px-10">
-          <h2 class="md:text-6 font-clash text-4xl leading-[44px] mb-4 text-violet-color">
-            {{ product?.title }}
-            <span>
-              <img
-                v-if="user?.user_role === 'admin'"
-                src="@/assets/icons/pencil.svg"
-                class="w-[20px] ml-3 pb-2 cursor-pointer"
-                alt="edit"
-                @click="editMode = true"
-              >
-              <img
-                v-if="user?.user_role === 'admin'"
-                src="@/assets/icons/plus.svg"
-                class="w-[30px] ml-3 pb-2 cursor-pointer"
-                alt="add-product"
-                @click="createNewProduct"
-              >
-            </span>
-          </h2>
-
-          <span class="md:text-[20px] block text-2xl mb-7 text-[#12131A]">£{{ product?.price }}</span>
-          <div class="text-grey-violet space-y-3">
-            <span class="font-clash text-violet-color mb-4 leading-5">Description</span>
+          <div v-else>
             <p class="block mb-[20px] leading-[22px]">
               {{ product?.description }}
               A timeless design, with premium materials features as one of our most popular and
@@ -161,45 +73,92 @@ he<template>
               <li>Quality timeless classic</li>
             </ul>
           </div>
-          <div class="mt-12 mb-4">
-            <span class="font-clash text-violet-color mb-4 leading-5">Dimensions</span>
-            <div class="pt-6 flex justify-between max-w-[241px] text-violet-color">
-              <div v-if="product?.width !== null" class="space-y-3">
-                <span class="font-clash text-sm text-grey-violet">Width</span>
-                <p>{{ product?.width }}</p>
+        </div>
+        <div class="mt-12 mb-4">
+          <span class="font-clash text-violet-color mb-4 leading-5">Dimensions</span>
+          <div class="pt-6 flex justify-between max-w-[241px] text-violet-color">
+            <div v-if="product?.width !== null" class="space-y-3">
+              <span class="font-clash text-sm text-grey-violet">Width</span>
+              <div v-if="editMode" class="flex">
+                <img src="@/assets/icons/pencil.svg" class=" w-[10px] mr-1" alt="edit">
+                <input
+                  v-model="payload.width"
+                  type="text"
+                >
               </div>
-              <div v-if="product?.height !== null" class="space-y-3">
-                <span class="font-clash text-sm text-grey-violet">Height</span>
-                <p>{{ product?.height }}</p>
+              <p v-else>{{ product?.width }}</p>
+            </div>
+
+            <div v-if="product?.height !== null" class="space-y-3">
+              <span class="font-clash text-sm text-grey-violet">Height</span>
+              <div v-if="editMode" class="flex">
+                <img src="@/assets/icons/pencil.svg" class=" w-[10px] mr-1" alt="edit">
+                <input
+                  v-model="payload.height"
+                  type="text"
+                >
               </div>
-              <div v-if="product?.depth !== null" class="space-y-3">
-                <span class="font-clash text-sm text-grey-violet">Depth</span>
-                <p>{{ product?.depth }}</p>
+              <p v-else>{{ product?.height }}</p>
+            </div>
+
+            <div v-if="product?.depth !== null" class="space-y-3">
+              <span class="font-clash text-sm text-grey-violet">Depth</span>
+              <div v-if="editMode" class="flex">
+                <img src="@/assets/icons/pencil.svg" class=" w-[10px] mr-1" alt="edit">
+                <input
+                  v-model="payload.depth"
+                  type="text"
+                >
               </div>
+              <p v-else>{{ product?.depth }}</p>
             </div>
           </div>
-          <div class="sm:flex-col lg:mb-4 flex justify-between mt-[27px] space-y-3.5">
-            <div class="sm:flex-col flex justify-left space-y-3.5 font-clash text-violet-color">
-              <span class="sm:mt-0 pt-[13px] pr-[22px] mt-[13px] leading-5">Quantity:</span>
-              <div class="sm:w-full w-[122px] h-[46px] flex justify-center items-center bg-light-grey">
-                <span class="text-light-grey-icon cursor-pointer" @click="changeQuantity('minus')">-</span>
-                <span class="mx-8"> {{ quantity }} </span>
-                <span class="text-light-grey-icon cursor-pointer" @click="changeQuantity('plus')">+</span>
-              </div>
+        </div>
+        <div v-if="editMode" class="sm:w-full w-[122px] h-[46px] mt-[26px] flex items-center">
+          <img src="@/assets/icons/pencil.svg" class=" w-[10px] mr-1" alt="edit">
+          <span class="sm:mt-0 w-[130px]  pr-[22px] leading-5">Quantity:</span>
+          <input
+            v-model="payload.qty"
+            type="text"
+          >
+        </div>
+        <div v-else class="sm:flex-col lg:mb-4 flex justify-between mt-[27px] space-y-3.5">
+          <div class="sm:flex-col flex justify-left space-y-3.5 font-clash text-violet-color">
+            <span class="sm:mt-0 pt-[13px] pr-[22px] mt-[13px] leading-5">Quantity:</span>
+            <div class="sm:w-full w-[122px] h-[46px] flex justify-center items-center bg-light-grey">
+              <span class="text-light-grey-icon cursor-pointer" @click="changeQuantity('minus')">-</span>
+              <span class="mx-8"> {{ quantity }} </span>
+              <span class="text-light-grey-icon cursor-pointer" @click="changeQuantity('plus')">+</span>
             </div>
-            <el-button
-              :type="$elComponentType.primary"
-              class="sm:w-full w-[143px] h-[56px] font-satoshi font-normal text-base cursor-pointer"
-              @click="basketStore.addProducts(product as IBasketProduct, quantity)"
-            >
-              Add to cart
-            </el-button>
           </div>
-          <div class="mt-2" />
+          <el-button
+            :type="$elComponentType.primary"
+            class="sm:w-full w-[143px] h-[56px] font-satoshi font-normal text-base cursor-pointer"
+            @click="basketStore.addProducts(product as IBasketProduct, quantity)"
+          >
+            Add to cart
+          </el-button>
+        </div>
+        <div v-if="editMode" class="flex justify-center mt-8">
+          <img
+            v-if="user?.user_role === 'admin'"
+            src="@/assets/icons/cancel.svg"
+            alt="cancel"
+            class="cursor-pointer"
+            @click="cancel"
+          >
+          <img
+            v-if="user?.user_role === 'admin'"
+            src="@/assets/icons/checkmark_green.svg"
+            alt="update"
+            class="cursor-pointer"
+            @click="handleUpdate(product?.id as string)"
+          >
         </div>
       </div>
-    </template>
+    </div>
   </section>
+
   <div class="container md:p-6 pt-20 px-20">
     <div class="md:text-[20px] mb-[33px] font-clash text-violet-color text-3xl">You might also like</div>
     <ProductsCollection />
